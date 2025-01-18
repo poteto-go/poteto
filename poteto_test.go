68d04c84c5fd3850d8268838f1c113056a8db8e8
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-01-18 15:00:14 +0000

    TEST: add test case

--- poteto_test.go
+++ poteto_test.go
@@ -136,6 +136,38 @@ func TestRunTLS(t *testing.T) {
 	}
 }
 
+func TestRunTLSStartUpWorkflows(t *testing.T) {
+	cert, _ := os.ReadFile("./_fixture/certs/cert.pem")
+	key, _ := os.ReadFile("./_fixture/certs/key.pem")
+
+	p := New()
+
+	isCalled := false
+	calledFunc := func() error {
+		isCalled = true
+		return nil
+	}
+
+	p.RegisterWorkflow(constant.START_UP_WORKFLOW, 1, calledFunc)
+
+	errChan := make(chan error)
+	go func() {
+		errChan <- p.RunTLS("8080", cert, key)
+	}()
+
+	select {
+	case <-time.After(500 * time.Millisecond):
+		if !isCalled {
+			t.Errorf("Unmatched")
+		}
+		if err := p.Stop(stdContext.Background()); err != nil {
+			t.Errorf("Unmatched")
+		}
+	case <-errChan:
+		return
+	}
+}
+
 func TestCertParseError(t *testing.T) {
 	cert := []byte("hello")
 	key, _ := os.ReadFile("./_fixture/certs/key.pem")
@@ -207,6 +239,29 @@ func TestRunStartUpWorkflows(t *testing.T) {
 		if !isCalled {
 			t.Errorf("Unmatched")
 		}
+		p.Stop(stdContext.Background())
+	}
+}
+
+func TestRunStartUpWorkflowsError(t *testing.T) {
+	p := New()
+	calledFunc := func() error {
+		return errors.New("error")
+	}
+
+	p.RegisterWorkflow(constant.START_UP_WORKFLOW, 1, calledFunc)
+
+	errChan := make(chan error)
+	go func() {
+		errChan <- p.Run("95")
+	}()
+
+	select {
+	case <-time.After(500 * time.Millisecond):
+		t.Errorf("Unmatched")
+		p.Stop(stdContext.Background())
+	case <-errChan:
+		// Pass case
 		p.Stop(stdContext.Background())
 	}
 }
