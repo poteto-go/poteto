6fd8032e25f2ba0f2612ee45aac2e274a8b3de1b
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-01-26 13:12:01 +0000

    DOC: on env setting

--- poteto_option.go
+++ poteto_option.go
@@ -1,5 +1,9 @@
 package poteto
 
+// ENV:
+//   WITH_REQUEST_ID: bool [true]
+//   DEBUG_MODE: bool [false]
+//   LISTENER_NETWORK: string [tcp]
 type PotetoOption struct {
 	WithRequestId   bool   `yaml:"with_request_id" env:"WITH_REQUEST_ID" envDefault:"true"`
 	DebugMode       bool   `yaml:"debug_mode" env:"DEBUG_MODE" envDefault:"false"`
