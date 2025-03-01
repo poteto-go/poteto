4c326a82a6eac2cc48aceffdb8df4ea9746f5c89
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-03-01 07:02:02 +0000

    REL: prepare -> 1.6.1

--- constant/constants.go
+++ constant/constants.go
@@ -1,6 +1,6 @@
 package constant
 
-const Version = "v1.6.0"
+const Version = "v1.6.1"
 
 const (
 	// max length of domain /
--- constant/old_constants.go
+++ constant/old_constants.go
@@ -1,6 +1,6 @@
 package constant
 
-const VERSION = "v1.6.0"
+const VERSION = "v1.6.1"
 
 const (
 	MAX_DOMAIN_LENGTH      int    = 255
