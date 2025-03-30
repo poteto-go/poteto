f544c47874bbe2edb633164e8c51638893262b34
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-03-30 04:29:03 +0000

    ref: ut jwt -> fixture

--- middleware/oidc_test.go
+++ middleware/oidc_test.go
@@ -3,6 +3,7 @@ package middleware_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/goccy/go-json"
@@ -18,7 +19,8 @@ func TestMiddleware_OidcWithConfig(t *testing.T) {
 	)
 
 	t.Run("valid token", func(t *testing.T) {
-		token := "eyJhbGciOiJSUzI1NiIsImtpZCI6ImtleUlkIiwidHlwIjoiSldUIn0.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiJleG1hcGxlLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiZXhtYXBsZS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwMDAwMDAwMDAwMDAwMDAiLCJlbWFpbCI6InRlc3RAZXhtYXBsZS5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6Imhhc2giLCJuYW1lIjoiVGVzdCBVc2VyIiwicGljdHVyZSI6Imh0dHBzOi8vcGljdHVyZSIsImdpdmVuX25hbWUiOiJUZXN0IiwiZmFtaWx5X25hbWUiOiJVc2VyIiwiaWF0IjoxNzQzMDc2MTk4LCJleHAiOjE3NDMwNzk3OTh9.pPriv3JvTgtQectH3mfOcMSO6T2RmWOMjyCXl4qd5_2tZNLfUh1M4f2JAqyectfrS1c4515k92_kKRN5985GHS78etadEH-0lFW7T3ehfYD5fs0HVo0EwYlDTg_8tZZ4x8kWd_RPdg21BQiubnHWcIpFv-HyMI9mWrkCmw31bkMrS-5a5-CMfeZwh2kJaD8D1_84LLuQuzbzIEBXoDlWCwHVkMTx6MSXNJpSqakCcDpE_sZ1WMg4r2jNvsG3nT-jSKwTMZ4g960YfYW0BAQafUKKDOZM9JnQ0HrFHEnKAun83AHjjmGVu3bq7Jc4IqQ5sYVuZD7BeciaEvQKvsk3IA"
+		tokenBytes, _ := os.ReadFile("../_fixture/token/jwt.txt")
+		token := string(tokenBytes)
 
 		w := httptest.NewRecorder()
 		req := httptest.NewRequest("GET", "/", nil)
