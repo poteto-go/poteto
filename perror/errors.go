ec02ee49f3eb1f3c8ddc9d2a91c7bf5661cac7ca
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-05-31 11:50:21 +0900

    ref: reafactor leaf

--- perror/errors.go
+++ perror/errors.go
@@ -7,4 +7,5 @@ var (
 	ErrNotApplicationJson      = errors.New("content-type is not application/json header")
 	ErrPathTraversalNotAllowed = errors.New("path traversal not allowed")
 	ErrPathLengthExceeded      = errors.New("path length exceeded")
+	ErrUnSupportedHTTPMethod   = errors.New("unsupported http method")
 )
