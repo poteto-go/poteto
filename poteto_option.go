bdc581c56a4303e67a2648d0758881dfa49a9312
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-06-26 00:01:38 +0900

    ref: ref by gemeini. refs: #322

--- poteto_option.go
+++ poteto_option.go
@@ -5,7 +5,8 @@ package poteto
 //   DEBUG_MODE: bool [false]
 //   LISTENER_NETWORK: string [tcp]
 type PotetoOption struct {
-	WithRequestId   bool   `yaml:"with_request_id" env:"WITH_REQUEST_ID" envDefault:"true"`
-	DebugMode       bool   `yaml:"debug_mode" env:"DEBUG_MODE" envDefault:"false"`
-	ListenerNetwork string `yaml:"listener_network" env:"LISTENER_NETWORK" envDefault:"tcp"`
+	WithRequestId      bool   `yaml:"with_request_id" env:"WITH_REQUEST_ID" envDefault:"true"`
+	DebugMode          bool   `yaml:"debug_mode" env:"DEBUG_MODE" envDefault:"false"`
+	ListenerNetwork    string `yaml:"listener_network" env:"LISTENER_NETWORK" envDefault:"tcp"`
+	MaxQueryParamCount int    `yaml:"max_query_param_count" env:"MAX_QUERY_PARAM_COUNT" envDefault:"32"`
 }
